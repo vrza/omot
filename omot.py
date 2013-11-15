48c11d87a20b89bd824ee477b1af7965b80f256e
Vladimir Vrzić <vvrzic@gmail.com>
2013-11-15 23:11:18 +0100

    Refactor logging init code into a procedure.

--- omot.py
+++ omot.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 #
-import pygtk
-pygtk.require('2.0')
-import gtk
 
-from omot import omotgtk
+
+def configure_logging():
+    import logging
+    from omot import config
+    if config.options.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
 
 if __name__ == "__main__":
+    configure_logging()
+    import pygtk
+    pygtk.require('2.0')
+    import gtk
+    from omot import omotgtk
     omotgtk.OmotGtk()
     gtk.main()
 
